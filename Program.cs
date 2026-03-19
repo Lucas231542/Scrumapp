455419d577056a022bc72d799d2bf3482f62e344
Aleksandar Nakic <aln007@edu.zealand.dk>
2026-03-19 09:46:24 +0100

    jj

--- Program.cs
+++ Program.cs
@@ -1,8 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine( "young shooter" );
-Console.WriteLine("KAJ");
-Console.WriteLine( "young shooter" );
-Console.WriteLine(":))))");
-Console.WriteLine( "young shooter" );
+jjj
 
