739ce030a06b9be5dcc50e4b182a6b1c1a13a97f
Anton Alfred Vilken Olesen <ano010@edu.zealand.dk>
2026-03-19 09:58:11 +0100

    ya

--- Program.cs
+++ Program.cs
@@ -1,6 +1,1 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine( "lillemand" );
-Console.WriteLine("KAJ");
-Console.WriteLine( "shisko" );
-Console.WriteLine(":))))");
